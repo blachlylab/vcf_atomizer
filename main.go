41529f254b78f8b834034b4d1c421c49c4d06377
cgregory <charles.gregory940@topper.wku.edu>
2019-02-13 11:26:25 -0500

    goformat

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"github.com/brentp/vcfgo"
-	"os"
-	"io"
-	"encoding/json"
 	"bufio"
-	"reflect"
-	"flag"
-	"strings"
-	"strconv"
 	"compress/gzip"
+	"encoding/json"
+	"flag"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/brentp/vcfgo"
+
 	//"fmt"
 	"fmt"
 )
 
-func annfield(anns []string)[]map[string]interface{}{
+func annfield(anns []string) []map[string]interface{} {
 	var field_list = []string{"allele",
 		"effect",
 		"impact",
@@ -31,7 +33,7 @@ func annfield(anns []string)[]map[string]interface{}{
 		"cds_position",
 		"protein_position",
 		"distance_to_feature",
-		"errors_warnings_info" }
+		"errors_warnings_info"}
 
 	//Decodes one or more annotations from VCF ANN field standard 1.0
 	//http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
@@ -42,30 +44,30 @@ func annfield(anns []string)[]map[string]interface{}{
 	//a corresponding key in the dict
 
 	// if empty string
-	if len(anns)<1{
+	if len(anns) < 1 {
 		return []map[string]interface{}{}
 	}
 	// specs page 2:
 	// Multiple effects / consequences are separated by comma.
 	var ann_string string
 	var ann_arr []string
-	var res= make([]map[string]interface{},0)
+	var res = make([]map[string]interface{}, 0)
 	var effects []string
-	for _, ann_string = range anns{
-		ann_arr =strings.Split(ann_string,"|")
-		effects=strings.Split(ann_arr[1],"&")
+	for _, ann_string = range anns {
+		ann_arr = strings.Split(ann_string, "|")
+		effects = strings.Split(ann_arr[1], "&")
 		var i int
-		var val,effect string
-		for _,effect=range effects{
-		    var eff_map=make(map[string]interface{})
-			for i,val=range ann_arr {
-				if i ==1{
-					eff_map["ANN_"+field_list[i]]=effect
-				}else if val!=""{
-					eff_map["ANN_"+field_list[i]]=val
+		var val, effect string
+		for _, effect = range effects {
+			var eff_map = make(map[string]interface{})
+			for i, val = range ann_arr {
+				if i == 1 {
+					eff_map["ANN_"+field_list[i]] = effect
+				} else if val != "" {
+					eff_map["ANN_"+field_list[i]] = val
 				}
 			}
-			res=append(res, eff_map)
+			res = append(res, eff_map)
 		}
 	}
 	return res
@@ -73,71 +75,71 @@ func annfield(anns []string)[]map[string]interface{}{
 
 //unpacks maps into main map
 //similar to ** operator in python
-func unpack(main map[string]interface{}, maps ...map[string]interface{}){
+func unpack(main map[string]interface{}, maps ...map[string]interface{}) {
 	var temp map[string]interface{}
-	for _,temp=range maps{
+	for _, temp = range maps {
 		var k string
 		var v interface{}
-		for k,v=range temp{
-			main[k]=v
+		for k, v = range temp {
+			main[k] = v
 		}
 	}
 }
 
-func vcf_transform(filename string,mapping string,meta string,sr bool)  {
+func vcf_transform(filename string, mapping string, meta string, sr bool) {
 	//Opens vcf and loops over rows
 	f, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	var r io.Reader
-	r,err=gzip.NewReader(f)
-	if err!=nil{
-		r=io.Reader(f)
+	r, err = gzip.NewReader(f)
+	if err != nil {
+		r = io.Reader(f)
 	}
 	vr, err := vcfgo.NewReader(r, false)
 	if err != nil {
-		// panic(err)
+		panic(err)
 	}
 	var variant *vcfgo.Variant
-	var out=bufio.NewWriter(os.Stdout)
-	var encoder=json.NewEncoder(out)
-	if mapping!=""{
-		j,_:=os.Create(mapping)
-		var map_out=bufio.NewWriter(j)
-		var map_encoder=json.NewEncoder(map_out)
-		json_map:=make(map[string]map[string]map[string]string)
-		json_map["properties"]=make(map[string]map[string]string)
-		for key,val:=range vr.Header.Infos{
-			key = "INFO_"+key
-			json_map["properties"][key]=make(map[string]string)
+	var out = bufio.NewWriter(os.Stdout)
+	var encoder = json.NewEncoder(out)
+	if mapping != "" {
+		j, _ := os.Create(mapping)
+		var map_out = bufio.NewWriter(j)
+		var map_encoder = json.NewEncoder(map_out)
+		json_map := make(map[string]map[string]map[string]string)
+		json_map["properties"] = make(map[string]map[string]string)
+		for key, val := range vr.Header.Infos {
+			key = "INFO_" + key
+			json_map["properties"][key] = make(map[string]string)
 			switch val.Type {
 			case "Integer":
-				json_map["properties"][key]["type"]="long"
+				json_map["properties"][key]["type"] = "long"
 			case "Float":
-				json_map["properties"][key]["type"]="float"
+				json_map["properties"][key]["type"] = "float"
 			case "Flag":
-				json_map["properties"][key]["type"]="boolean"
+				json_map["properties"][key]["type"] = "boolean"
 			default:
-				delete(json_map["properties"],key)
+				delete(json_map["properties"], key)
 			}
 
 		}
 		map_encoder.Encode(json_map)
 	}
-	if meta!=""{
-		fi,_:=os.Create(meta)
-		var meta_out=bufio.NewWriter(fi)
-		var meta_encoder=json.NewEncoder(meta_out)
-		for _,s:=range vr.Header.SampleNames{
-			meta_map:=make(map[string]interface{})
-			meta_map["sample"]=s
-			meta_map["type"]="variant_meta"
-			meta_map["extras"]=vr.Header.Extras
-			meta_map["file_format"]=vr.Header.FileFormat
-			meta_map["filters"]=vr.Header.Filters
-			meta_map["contigs"]=vr.Header.Contigs
-			meta_map["infos"]=vr.Header.Infos
+	if meta != "" {
+		fi, _ := os.Create(meta)
+		var meta_out = bufio.NewWriter(fi)
+		var meta_encoder = json.NewEncoder(meta_out)
+		for _, s := range vr.Header.SampleNames {
+			meta_map := make(map[string]interface{})
+			meta_map["sample"] = s
+			meta_map["type"] = "variant_meta"
+			meta_map["extras"] = vr.Header.Extras
+			meta_map["file_format"] = vr.Header.FileFormat
+			meta_map["filters"] = vr.Header.Filters
+			meta_map["contigs"] = vr.Header.Contigs
+			meta_map["infos"] = vr.Header.Infos
 			meta_encoder.Encode(meta_map)
 		}
 		meta_out.Flush()
@@ -145,36 +147,36 @@ func vcf_transform(filename string,mapping string,meta string,sr bool)  {
 	}
 	for {
 		variant = vr.Read()
-		if variant==nil{
+		if variant == nil {
 			break
 		}
-		parse_vcf_record(variant,encoder,sr)
+		parse_vcf_record(variant, encoder, sr)
 
 	}
 	out.Flush()
 }
-func parse_vcf_field_array(val string, val_type string)[]interface{}{
-	var str_arr=strings.Split(val,",")
+func parse_vcf_field_array(val string, val_type string) []interface{} {
+	var str_arr = strings.Split(val, ",")
 	var ret []interface{}
 	var str string
-	for _,str=range str_arr{
+	for _, str = range str_arr {
 		switch val_type {
 		case "Integer":
-			var i,err=strconv.Atoi(str)
-			if err!=nil{
+			var i, err = strconv.Atoi(str)
+			if err != nil {
 				panic(err)
-			}else {
-				ret=append(ret,i)
+			} else {
+				ret = append(ret, i)
 			}
 		case "Float":
-			var i,err=strconv.ParseFloat(str, 64)
-			if err!=nil{
+			var i, err = strconv.ParseFloat(str, 64)
+			if err != nil {
 				panic(err)
-			}else {
-				ret=append(ret,i)
+			} else {
+				ret = append(ret, i)
 			}
 		case "String":
-			ret=append(ret,str)
+			ret = append(ret, str)
 		default:
 			panic(val_type)
 		}
@@ -182,44 +184,44 @@ func parse_vcf_field_array(val string, val_type string)[]interface{}{
 	return ret
 }
 
-func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
+func parse_vcf_record(variant *vcfgo.Variant, encoder *json.Encoder, sr bool) {
 	//assign fields common to the row
 	var common_fields = make(map[string]interface{})
-	common_fields["type"]="variant_vcf"
-	common_fields["CHROM"]=variant.Chromosome
-	common_fields["POS"]=variant.Pos
-	common_fields["REF"]=variant.Reference
-	common_fields["QUAL"]=variant.Quality
-	common_fields["ID"]=variant.Id_
-	common_fields["FILTER"]=strings.Split(variant.Filter,";")
+	common_fields["type"] = "variant_vcf"
+	common_fields["CHROM"] = variant.Chromosome
+	common_fields["POS"] = variant.Pos
+	common_fields["REF"] = variant.Reference
+	common_fields["QUAL"] = variant.Quality
+	common_fields["ID"] = variant.Id_
+	common_fields["FILTER"] = strings.Split(variant.Filter, ";")
 	var info_key string
 	var anns []map[string]interface{}
 	var ann_strings []string
 	//parse the info fields
-	if !(len(variant.Info().Keys())==1&&variant.Info().Keys()[0]==""){
-		for _,info_key=range variant.Info().Keys(){
-			var res,_=variant.Info().Get(info_key)
-			var s=reflect.ValueOf(res)
+	if !(len(variant.Info().Keys()) == 1 && variant.Info().Keys()[0] == "") {
+		for _, info_key = range variant.Info().Keys() {
+			var res, _ = variant.Info().Get(info_key)
+			var s = reflect.ValueOf(res)
 			var err error
-			if info_key=="ANN"{
-				ann_strings=make([]string, s.Len())
-				for i := 0; i < s.Len(); i++{
-					ann_strings[i]=s.Index(i).String()
+			if info_key == "ANN" {
+				ann_strings = make([]string, s.Len())
+				for i := 0; i < s.Len(); i++ {
+					ann_strings[i] = s.Index(i).String()
 				}
-				if s.Type().Name()=="string" {
-					anns=annfield([]string{s.String()})
-				}else{
-					anns=annfield(ann_strings)
+				if s.Type().Name() == "string" {
+					anns = annfield([]string{s.String()})
+				} else {
+					anns = annfield(ann_strings)
 				}
-			}else{
-				if variant.Header.Infos[info_key].Number!="1"{
+			} else {
+				if variant.Header.Infos[info_key].Number != "1" {
 					common_fields["INFO_"+info_key] = res
-				}else {
+				} else {
 					switch key_type := variant.Header.Infos[info_key].Type; key_type {
 					case "Integer":
-						common_fields["INFO_"+info_key]= s.Int()
+						common_fields["INFO_"+info_key] = s.Int()
 					case "Float":
-						common_fields["INFO_"+info_key]= s.Float()
+						common_fields["INFO_"+info_key] = s.Float()
 					case "String":
 						common_fields["INFO_"+info_key] = s.String()
 					case "Flag":
@@ -228,7 +230,7 @@ func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
 						panic(res)
 					}
 				}
-				if err!=nil{
+				if err != nil {
 					panic(err)
 				}
 			}
@@ -237,18 +239,18 @@ func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
 	var alt string
 	var index int
 	//loop over alts
-	for index,alt=range variant.Alt() {
-		var alt_fields= make(map[string]interface{})
+	for index, alt = range variant.Alt() {
+		var alt_fields = make(map[string]interface{})
 		alt_fields["ALT"] = alt
 		if len(variant.Samples) < 1 {
-			if len(anns)<1{
+			if len(anns) < 1 {
 				unpack(alt_fields, common_fields)
 				encoder.Encode(alt_fields)
 				continue
-			}else{
+			} else {
 				var ann map[string]interface{}
 				for _, ann = range anns {
-					if (reflect.ValueOf(ann["ANN_allele"]).String() == alt) {
+					if reflect.ValueOf(ann["ANN_allele"]).String() == alt {
 						unpack(ann, common_fields, alt_fields)
 						encoder.Encode(ann)
 					}
@@ -259,7 +261,7 @@ func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
 		}
 		//loop over samples
 		for samidx, sample := range variant.Samples {
-			var found= false
+			var found = false
 			var gt int
 			for _, gt = range sample.GT {
 				if index+1 == gt {
@@ -269,20 +271,20 @@ func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
 			if !found {
 				continue
 			}
-			var sample_fields= make(map[string]interface{})
+			var sample_fields = make(map[string]interface{})
 			sample_fields["sample"] = variant.Header.SampleNames[samidx]
 			var key, val string
 			var err error
 			for key, val = range sample.Fields {
-				if key != "DP" && key !="GT" &&key !="MQ" && key !="GL" && key !="GQ" && key !="AD"{
-					if variant.Header.SampleFormats[key].Number!="1"{
-						sample_fields[key]=parse_vcf_field_array(val,variant.Header.SampleFormats[key].Type)
-					}else{
+				if key != "DP" && key != "GT" && key != "MQ" && key != "GL" && key != "GQ" && key != "AD" {
+					if variant.Header.SampleFormats[key].Number != "1" {
+						sample_fields[key] = parse_vcf_field_array(val, variant.Header.SampleFormats[key].Type)
+					} else {
 						switch key_type := variant.Header.SampleFormats[key].Type; key_type {
 						case "Integer":
 							sample_fields[key], err = strconv.Atoi(val)
 						case "Float":
-							if val !="."{
+							if val != "." {
 								sample_fields[key], err = strconv.ParseFloat(val, 64)
 							}
 						case "String":
@@ -291,55 +293,55 @@ func parse_vcf_record(variant *vcfgo.Variant,encoder *json.Encoder,sr bool)  {
 							panic(key_type)
 						}
 					}
-					if err!=nil{
+					if err != nil {
 						panic(err)
 					}
 				}
 			}
-			sample_fields["DP"]=sample.DP
-			if len(sample.GT)>0{
-				sample_fields["GT"]=sample.GT
+			sample_fields["DP"] = sample.DP
+			if len(sample.GT) > 0 {
+				sample_fields["GT"] = sample.GT
 			}
-			sample_fields["MQ"]=sample.MQ
-			if len(sample.GL)>0{
-				sample_fields["GL"]=sample.GL
+			sample_fields["MQ"] = sample.MQ
+			if len(sample.GL) > 0 {
+				sample_fields["GL"] = sample.GL
 			}
-			sample_fields["GQ"]=sample.GQ
-			sample_fields["Ref_Depth"],_=sample.RefDepth()
-			sample_fields["Alt_depths"],_=sample.AltDepths()
+			sample_fields["GQ"] = sample.GQ
+			sample_fields["Ref_Depth"], _ = sample.RefDepth()
+			sample_fields["Alt_depths"], _ = sample.AltDepths()
 			var ann map[string]interface{}
 			//for each sample loop over anns and link
-			if len(anns)<1{
+			if len(anns) < 1 {
 				unpack(sample_fields, common_fields, alt_fields)
 				encoder.Encode(sample_fields)
 			}
-            if sr{
-                unpack(sample_fields, common_fields, alt_fields)
-                sample_fields["ANN"]=anns
-                encoder.Encode(sample_fields)
-            }else{
-                for _, ann = range anns {
-                    if (reflect.ValueOf(ann["ANN_allele"]).String() == alt) {
-                        unpack(ann, sample_fields, common_fields, alt_fields)
-                        encoder.Encode(ann)
-                    }
-                }
+			if sr {
+				unpack(sample_fields, common_fields, alt_fields)
+				sample_fields["ANN"] = anns
+				encoder.Encode(sample_fields)
+			} else {
+				for _, ann = range anns {
+					if reflect.ValueOf(ann["ANN_allele"]).String() == alt {
+						unpack(ann, sample_fields, common_fields, alt_fields)
+						encoder.Encode(ann)
+					}
+				}
 
-            }
+			}
 		}
 	}
 }
 
 func main() {
-	var mapping=flag.String("mapping","","print predicted elasticsearch mapping")
-	var meta=flag.String("meta","","write metadata to file")
-	var atom=flag.Bool("atom",false,"write Aegis atomizer.py config")
-    var sr=flag.Bool("one",false,"do not permute rows by annotations")
+	var mapping = flag.String("mapping", "", "print predicted elasticsearch mapping")
+	var meta = flag.String("meta", "", "write metadata to file")
+	var atom = flag.Bool("atom", false, "write Aegis atomizer.py config")
+	var sr = flag.Bool("one", false, "do not permute rows by annotations")
 	flag.Parse()
-	if *atom{
+	if *atom {
 		fmt.Println("atomizer [flags] [input]")
 		os.Exit(0)
 	}
-	var filename=flag.Arg(0)
-	vcf_transform(filename,*mapping,*meta,*sr)
+	var filename = flag.Arg(0)
+	vcf_transform(filename, *mapping, *meta, *sr)
 }
